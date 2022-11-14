7884bec2cb64423031043b87457445c519f06cae
Nhan <nguyenngocbaonhan2003@gmail.com>
2022-11-14 18:48:32 +0700

    ss2 module 02

--- module_2/src/ss01_introduction/bai_tap/readNumber.java
+++ module_2/src/ss01_introduction/bai_tap/readNumber.java
@@ -3,133 +3,239 @@ package ss01_introduction.bai_tap;
 import java.util.Scanner;
 
 public class readNumber {
-    public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        System.out.println("Enter the number: ");
-        String strNumber = scanner.nextLine();
-        String[] numToArr = strNumber.split("");
-        for (int i = 0; i < strNumber.length(); i++) {
-            System.out.println(numToArr[i]);
+    static  void readOnes(String number) {
+        switch (number) {
+            case "0" -> {
+                System.out.println("");
+            }
+            case "1" -> {
+                System.out.println("one");
+            }
+            case "2" -> {
+                System.out.println("two");
+            }
+            case "3" -> {
+                System.out.println("three");
+            }
+            case "4" -> {
+                System.out.println("four");
+            }
+            case "5" -> {
+                System.out.println("five");
+            }
+            case "6" -> {
+                System.out.println("six");
+            }
+            case "7" -> {
+                System.out.println("seven");
+            }
+            case "8" -> {
+                System.out.println("eight");
+            }
+            case "9" -> {
+                System.out.println("nine");
+            }
         }
-        int length = strNumber.length();
-        switch (length) {
-            case 1:
-                switch (numToArr[0]) {
-                    case "0":
-                        System.out.println("zero");
-                        break;
-                    case "1":
-                        System.out.println("one");
-                        break;
-                    case "2":
-                        System.out.println("two");
-                        break;
-                    case "3":
-                        System.out.println("three");
-                        break;
-                    case "4":
-                        System.out.println("four");
-                        break;
-                    case "5":
-                        System.out.println("five");
-                        break;
-                    case "6":
-                        System.out.println("six");
-                        break;
-                    case "7":
-                        System.out.println("seven");
-                        break;
-                    case "8":
-                        System.out.println("eight");
-                        break;
-                    case "9":
-                        System.out.println("nine");
-                        break;
-                    default:
-                        System.out.println("out of ability");
-                        break;
+    }
+    static void readNumber(String number) {
+        String[] toArray = number.split("");
+        if (toArray.length < 1) {
+            System.out.println("out of ability");
+        } else if (toArray.length < 2) {
+            switch (toArray[0]) {
+                case "0" -> {
+                    System.out.println("zero");
                 }
-                break;
-            case 2:
-                switch (numToArr[0]) {
+                case "1" -> {
+                    System.out.println("one");
+                }
+                case "2" -> {
+                    System.out.println("two");
+                }
+                case "3" -> {
+                    System.out.println("three");
+                }
+                case "4" -> {
+                    System.out.println("four");
+                }
+                case "5" -> {
+                    System.out.println("five");
+                }
+                case "6" -> {
+                    System.out.println("six");
+                }
+                case "7" -> {
+                    System.out.println("seven");
+                }
+                case "8" -> {
+                    System.out.println("eight");
+                }
+                case "9" -> {
+                    System.out.println("nine");
+                }
+            }
+        } else if (toArray.length < 3) {
+            if (toArray[0].equals("1")) {
+                switch (toArray[1]) {
+                    case "0" -> {
+                        System.out.println("ten");
+                    }
                     case "1" -> {
-                        switch (numToArr[1]) {
-                            case "0" -> {
-                                System.out.println("ten");
-                            }
-                            case "1" -> {
-                                System.out.println("eleven");
-                            }
-                            case "2" -> {
-                                System.out.println("twelve");
-                            }
-                            case "3" -> {
-                                System.out.println("thirteen");
-                            }
-                            case "4" -> {
-                                System.out.println("fourteen");
-                            }
-                            case "5" -> {
-                                System.out.println("fifteen");
-                            }
-                            case "6" -> {
-                                System.out.println("sixteen");
-                            }
-                            case "7" -> {
-                                System.out.println("seventeen");
-                            }
-                            case "8" -> {
-                                System.out.println("eighteen");
-                            }
-                            case "9" -> {
-                                System.out.println("nineteen");
-                            }
-                        }
+                        System.out.println("eleven");
                     }
                     case "2" -> {
-                        System.out.print("twenty");
-                        switch (numToArr[1]) {
-                            case "0" -> {
-                                // nothing
-                            }
-                            case "1" -> {
-                                System.out.println("one");
-                            }
-                            case "2" -> {
-                                System.out.println("two");
-                            }
-                            case "3" -> {
-                                System.out.println("three");
-                            }
-                            case "4" -> {
-                                System.out.println("four");
-                            }
-                            case "5" -> {
-                                System.out.println("five");
-                            }
-                            case "6" -> {
-                                System.out.println("sixe");
-                            }
-                            case "7" -> {
-                                System.out.println("seven");
-                            }
-                            case "8" -> {
-                                System.out.println("eight");
-                            }
-                            case "9" -> {
-                                System.out.println("nine");
-                            }
-                        }
+                        System.out.println("twelve");
                     }
-                    default -> {
-                        System.out.println("out of ability");
+                    case "3" -> {
+                        System.out.println("thirteen");
+                    }
+                    case "4" -> {
+                        System.out.println("fourteen");
+                    }
+                    case "5" -> {
+                        System.out.println("fifteen");
+                    }
+                    case "6" -> {
+                        System.out.println("sixteen");
+                    }
+                    case "7" -> {
+                        System.out.println("seventeen");
+                    }
+                    case "8" -> {
+                        System.out.println("eighteen");
+                    }
+                    case "9" -> {
+                        System.out.println("nineteen");
                     }
                 }
-            case 3:
-
-            case 4:
-            default:
-                System.out.println("out of ability");
+            } else {
+                switch (toArray[0]) {
+                    case "2" -> {
+                        System.out.print("twenty ");
+                    }
+                    case "3" -> {
+                        System.out.print("thirty ");
+                    }
+                    case "4" -> {
+                        System.out.print("forty ");
+                    }
+                    case "5" -> {
+                        System.out.print("fifty ");
+                    }
+                    case "6" -> {
+                        System.out.print("sixty ");
+                    }
+                    case "7" -> {
+                        System.out.print("seventy ");
+                    }
+                    case "8" -> {
+                        System.out.print("eighty ");
+                    }
+                    case "9" -> {
+                        System.out.print("ninety ");
+                    }
+                }
+                readOnes(toArray[1]);
+            }
+        } else if (toArray.length < 4) {
+            switch (toArray[0]) {
+                case "1" -> {
+                    System.out.print("one hundred ");
+                }
+                case "2" -> {
+                    System.out.print("two hundreds ");
+                }
+                case "3" -> {
+                    System.out.print("three hundreds ");
+                }
+                case "4" -> {
+                    System.out.print("four hundreds ");
+                }
+                case "5" -> {
+                    System.out.print("five hundreds ");
+                }
+                case "6" -> {
+                    System.out.print("six hundreds ");
+                }
+                case "7" -> {
+                    System.out.print("seven hundreds ");
+                }
+                case "8" -> {
+                    System.out.print("eight hundreds ");
+                }
+                case "9" -> {
+                    System.out.print("nine hundreds ");
+                }
+            }
+            if (toArray[1].equals("1")) {
+                switch (toArray[2]) {
+                    case "0" -> {
+                        System.out.println("ten");
+                    }
+                    case "1" -> {
+                        System.out.println("eleven");
+                    }
+                    case "2" -> {
+                        System.out.println("twelve");
+                    }
+                    case "3" -> {
+                        System.out.println("thirteen");
+                    }
+                    case "4" -> {
+                        System.out.println("fourteen");
+                    }
+                    case "5" -> {
+                        System.out.println("fifteen");
+                    }
+                    case "6" -> {
+                        System.out.println("sixteen");
+                    }
+                    case "7" -> {
+                        System.out.println("seventeen");
+                    }
+                    case "8" -> {
+                        System.out.println("eighteen");
+                    }
+                    case "9" -> {
+                        System.out.println("nineteen");
+                    }
+                }
+            } else {
+                switch (toArray[1]) {
+                    case "2" -> {
+                        System.out.print("twenty ");
+                    }
+                    case "3" -> {
+                        System.out.print("thirty ");
+                    }
+                    case "4" -> {
+                        System.out.print("forty ");
+                    }
+                    case "5" -> {
+                        System.out.print("fifty ");
+                    }
+                    case "6" -> {
+                        System.out.print("sixty ");
+                    }
+                    case "7" -> {
+                        System.out.print("seventy ");
+                    }
+                    case "8" -> {
+                        System.out.print("eighty ");
+                    }
+                    case "9" -> {
+                        System.out.print("ninety ");
+                    }
+                }
+                readOnes(toArray[2]);
+            }
+        } else {
+            System.out.println("out of ability");
         }
+    }
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        readNumber("999");
+        readNumber("9999");
     }
 }
