331020a4151963d0c6acf090f6a9989d1514b337
Nhan <nguyenngocbaonhan2003@gmail.com>
2022-11-14 19:20:53 +0700

    ss2 module 02

--- module_2/src/ss02_loop/bai_tap/displayShape.java
+++ module_2/src/ss02_loop/bai_tap/displayShape.java
@@ -13,14 +13,14 @@ public class displayShape {
     static void printSquareTriangle(int heigth, int width) {
         // top-left
         for (int i = 0; i < heigth; i++) {
-            for (int j = 0; j < width; j++) {
+            for (int j = 1; j <= width; j++) {
                 if (i == 0) {
-                    System.out.print("* ");
+                    System.out.print("*");
                 } else {
-                    if (j < width - i) {
-                        System.out.print("* ");
-                    } else {
+                    if (j > width - i) {
                         System.out.print(" ");
+                    } else {
+                        System.out.print("*");
                     }
                 }
             }
@@ -29,27 +29,11 @@ public class displayShape {
 
         // top-right
         for (int i = 0; i < heigth; i++) {
-            for (int j = 0; j < width; j++) {
+            for (int j = 1; j <= width; j++) {
                 if (i == 0) {
-                    System.out.print("* ");
-                } else {
-                    if (j >= i) {
-                        System.out.print("* ");
-                    } else {
-                        System.out.print("   ");
-                    }
-                }
-            }
-            System.out.println("");
-        }
-
-        // bottom-left
-        for (int i = 0; i < heigth; i++) {
-            for (int j = 0; j < width; j++) {
-                if (i == heigth) {
                     System.out.print("*");
                 } else {
-                    if (j <= i) {
+                    if (j > i) {
                         System.out.print("*");
                     } else {
                         System.out.print(" ");
@@ -59,16 +43,30 @@ public class displayShape {
             System.out.println("");
         }
 
-        // bottom-right
-        for (int i = 1; i <= heigth; i++) {
+        // bottom-left
+        for (int i = 0; i < heigth; i++) {
             for (int j = 1; j <= width; j++) {
-                if (i == heigth) {
-                    System.out.print("* ");
+                if (i == heigth - 1) {
+                    System.out.print("*");
                 } else {
-                    if (j > width - i) {
-                        System.out.print("* ");
+                    if (j <= i + 1) {
+                        System.out.print("*");
+                    }
+                }
+            }
+            System.out.println("");
+        }
+
+        // bottom-right
+        for (int i = 0; i < heigth; i++) {
+            for (int j = 1; j <= width; j++) {
+                if (i == heigth - 1) {
+                    System.out.print("*");
+                } else {
+                    if (j >= width - i) {
+                        System.out.print("*");
                     } else {
-                        System.out.print("   ");
+                        System.out.print(" ");
                     }
                 }
             }
@@ -86,7 +84,7 @@ public class displayShape {
     }
     public static void main(String[] args) {
 //        printSolidRectangle(3, 15);
-//        printSquareTriangle(3, 4);
-        printIsoscelesTriangle(5);
+        printSquareTriangle(5, 5);
+//        printIsoscelesTriangle(5);
     }
 }
