d71f687b0acdfe377ac84ebcef4f40cb87187f4f
Nhan <nguyenngocbaonhan2003@gmail.com>
2023-02-22 18:40:24 +0700

    update cau 5 spl

--- case_study_module_03_mysql/cau_05.sql
+++ case_study_module_03_mysql/cau_05.sql
@@ -4,5 +4,32 @@ use furamaresort;
 -- (Với tổng tiền được tính theo công thức như sau: Chi Phí Thuê + Số Lượng * Giá, với Số Lượng và Giá là từ bảng dich_vu_di_kem, hop_dong_chi_tiet) cho tất cả các khách hàng đã từng đặt phòng. 
 -- (những khách hàng nào chưa từng đặt phòng cũng phải hiển thị ra).
 
-select 
-from khach_hang kh, loai_khach lk,  dich_vu dv, hop_dong hd, 
+select
+    kh.ma_khach_hang,
+    kh.ho_ten,
+    lk.ten_loai_khach,
+    hd.ma_hop_dong,
+    dv.ten_dich_vu,
+    hd.ngay_lam_hop_dong,
+    hd.ngay_ket_thuc,
+    (sum(dvdk.gia * hdct.so_luong) + tmp.gia_dv) tong_tien
+from
+    khach_hang kh
+    left join loai_khach lk using (ma_loai_khach)
+    left join hop_dong hd using (ma_khach_hang)
+    left join dich_vu dv using (ma_dich_vu)
+    left join hop_dong_chi_tiet hdct using (ma_hop_dong)
+    left join dich_vu_di_kem dvdk using (ma_dich_vu_di_kem)
+    left join (
+        select
+            kh.ma_khach_hang as ma_khach_hang,
+            sum(dv.chi_phi_thue) as gia_dv
+        from
+            khach_hang kh
+            join hop_dong hd using (ma_khach_hang)
+            join dich_vu dv using (ma_dich_vu)
+        group by
+            kh.ma_khach_hang
+    ) tmp using (ma_khach_hang)
+group by
+    kh.ma_khach_hang;
