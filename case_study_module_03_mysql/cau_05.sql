2fa82129bffde50b1862f58eeb151738564f79c0
Nhan <nguyenngocbaonhan2003@gmail.com>
2023-02-22 19:22:35 +0700

    update bai_05

--- case_study_module_03_mysql/cau_05.sql
+++ case_study_module_03_mysql/cau_05.sql
@@ -1,8 +1,10 @@
 use furamaresort;
 
 -- 5.	Hiển thị ma_khach_hang, ho_ten, ten_loai_khach, ma_hop_dong, ten_dich_vu, ngay_lam_hop_dong, ngay_ket_thuc, tong_tien 
--- (Với tổng tiền được tính theo công thức như sau: Chi Phí Thuê + Số Lượng * Giá, với Số Lượng và Giá là từ bảng dich_vu_di_kem, hop_dong_chi_tiet) cho tất cả các khách hàng đã từng đặt phòng. 
--- (những khách hàng nào chưa từng đặt phòng cũng phải hiển thị ra).
+-- 	(Với tổng tiền được tính theo công thức như sau: Chi Phí Thuê + Số Lượng * Giá, 
+-- 	với Số Lượng và Giá là từ bảng dich_vu_di_kem, hop_dong_chi_tiet) 
+-- 	cho tất cả các khách hàng đã từng đặt phòng. 
+-- 	(những khách hàng nào chưa từng đặt phòng cũng phải hiển thị ra).
 
 select
     kh.ma_khach_hang,
@@ -19,7 +21,7 @@ from
     left join hop_dong hd using (ma_khach_hang)
     left join dich_vu dv using (ma_dich_vu)
     left join hop_dong_chi_tiet hdct using (ma_hop_dong)
-    left join dich_vu_di_kem dvdk using (ma_dich_vu_di_kem)
+    left join dich_vu_di_kem dvdk on (dvdk.ma_dich_vu_di_kem = dv.ma_dich_vu)
     left join (
         select
             kh.ma_khach_hang as ma_khach_hang,
