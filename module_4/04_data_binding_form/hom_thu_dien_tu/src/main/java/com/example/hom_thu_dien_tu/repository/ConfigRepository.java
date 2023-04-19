3462c22c7c04a23123c8a8fa5c8ee1ae86a6328b
Nhan <nguyenngocbaonhan2003@gmail.com>
2023-04-19 21:16:04 +0700

    update

---
+++ module_4/04_data_binding_form/hom_thu_dien_tu/src/main/java/com/example/hom_thu_dien_tu/repository/ConfigRepository.java
@@ -0,0 +1,9 @@
+package com.example.hom_thu_dien_tu.repository;
+
+import com.example.hom_thu_dien_tu.model.Config;
+
+import java.util.List;
+
+public interface ConfigRepository {
+    List<Config> 
+}
---
+++ module_4/05_csdl_orm/ung_dung_nghe_nhac/src/main/java/com/example/ung_dung_nghe_nhac/model/Song.java
@@ -0,0 +1,9 @@
+package com.example.ung_dung_nghe_nhac.model;
+
+public class Song {
+    private int id;
+    private String name;
+    private String singer;
+    private String type;
+    private String link;
+}
