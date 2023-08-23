e911b759d704b2f2785ca40978c4b277fa3241ce
Nhan <62550213+NhanPz01@users.noreply.github.com>
2023-08-23 17:56:05 +0700

    Update App.js

--- module_5/1_tong_quan_react/sign-in-form/src/App.js
+++ module_5/1_tong_quan_react/sign-in-form/src/App.js
@@ -6,21 +6,21 @@ function App() {
     <div className="form-signin">
       <form>
         <img className="mb-4" src="https://upload.wikimedia.org/wikipedia/commons/thumb/b/b2/Bootstrap_logo.svg/2560px-Bootstrap_logo.svg.png" alt="" width="72" height="57" />
-        <h1 className="h3 mb-3 fw-normal">Please sign in</h1>
+        <h1 className="h3 mb-3 fw-normal">Vui lòng đăng nhập</h1>
         <div className="form-floating">
           <input type="email" className="form-control email" id="floatingInput" placeholder="name@example.com" />
-          <label>Email address</label>
+          <label>Email</label>
         </div>
         <div className="form-floating">
           <input type="password" className="form-control password" id="floatingPassword" placeholder="Password" />
-          <label>Password</label>
+          <label>Mật khẩu</label>
         </div>
         <div className="checkbox mb-3">
           <label>
-            <input type="checkbox" /> Remember me
+            <input type="checkbox" /> Lưu đăng nhập
           </label>
         </div>
-        <button className="w-100 btn btn-lg btn-primary" type="submit">Sign in</button>
+        <button className="w-100 btn btn-lg btn-primary" type="submit">Đăng nhập</button>
         <p className="mt-5 mb-3 text-muted">© 2017–2021</p>
       </form>
     </div>
