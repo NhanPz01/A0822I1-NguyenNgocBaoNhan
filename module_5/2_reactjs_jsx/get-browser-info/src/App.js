a83eaba39a95883866d90706978728f3b9c56658
Nhan <62550213+NhanPz01@users.noreply.github.com>
2023-10-23 19:03:31 +0700

    Create get browser info

---
+++ module_5/2_reactjs_jsx/get-browser-info/src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import BrowserInfo from './components/BrowserInfo';
+
+function App() {
+  return (
+    <div className="App">
+      <BrowserInfo/>
+    </div>
+  );
+}
+
+export default App;
---
+++ module_5/2_reactjs_jsx/get-browser-info/src/components/BrowserInfo.js
@@ -0,0 +1,8 @@
+
+export default function BrowserInfo() {
+  return (
+    <>
+      <h4>Browser's details: {navigator.userAgent}</h4>      
+    </>
+  )
+}
