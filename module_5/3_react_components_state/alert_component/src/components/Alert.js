7256d5e983e11f3c0196faddfa6ea27625225c06
Nhan <nguyenngocbaonhan2003@gmail.com>
2023-11-06 18:59:07 +0700

    alert component

---
+++ module_5/3_react_components_state/alert_component/src/components/Alert.js
@@ -0,0 +1,10 @@
+
+export default function Alert() {
+  return (
+    <>
+      <div className="alert alert-danger" role="alert">
+        Cảnh báo! Tài nguyên bạn vừa truy cập không tồn tại.
+      </div>
+    </>
+  )
+}
