32d9e4d3587b1f1b19fd3b99d8e263cf5a6fc9b6
Nhan <nguyenngocbaonhan2003@gmail.com>
2023-11-08 18:04:07 +0700

    create bootstrap login/logout

--- module_5/3_react_components_state/alert_component/src/components/Alert.js
+++ module_5/3_react_components_state/alert_component/src/components/Alert.js
@@ -1,9 +1,20 @@
+import { useEffect, useState } from "react"
 
 export default function Alert() {
+  const [count, setCount] = useState(0)
+
+
+
+  useEffect( () => {
+    setInterval( () => {
+      setCount(count + 1)
+    }, 1000)
+  }, [count])
+  
   return (
     <>
       <div className="alert alert-danger" role="alert">
-        Cảnh báo! Tài nguyên bạn vừa truy cập không tồn tại.
+        Cảnh báo {count}! Tài nguyên bạn vừa truy cập không tồn tại.
       </div>
     </>
   )
